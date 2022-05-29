a4492b738490a54dcef6c698f3b6ef90cec2219d
JFl12 <65743700+JFl12@users.noreply.github.com>
2022-05-28 20:05:37 -0400

    Uploaded PhoneAndEmail.py

---
+++ venv/PhoneAndEmail.py
@@ -0,0 +1,15 @@
+#! python3
+# phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
+
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?                   # area code
+    (\s|-|\.)?                           # seperator
+    (\d{3})                              # first  3 digits
+    (\s|-|\.)                            # seperator
+    (\d{4})                              # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?       # extension
+    )''', re.VERBOSE)
+
+
